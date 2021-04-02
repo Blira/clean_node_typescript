8753fe16c21132e51b1623083db1c0b89cfdc693
Bruno Lira <brunoelira@gmail.com>
2021-04-02 02:05:17 -0300

    chore: ignore env on jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  "coveragePathIgnorePatterns": [
+  coveragePathIgnorePatterns: [
     "<rootDir>/src/presentation/protocols/index.ts",
     "<rootDir>/src/presentation/controllers/signup/signup-protocols.ts",
     "<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts",
-    "<rootDir>/src/main/server.ts"
+    "<rootDir>/src/main/server.ts",
+    "<rootDir>/src/main/config/env.ts"
   ],
   preset: '@shelf/jest-mongodb',
   coverageDirectory: 'coverage',
